44f5e5b7279ca88ebc108d54a1e03c0fde04b758
why2fly <flyte2012@163.com>
2018-01-12 17:18:06 +0800

    新增一个system的demo,需要在console中查看打印结果;更新readme

---
+++ 5.4 系统信息 show_sys_info/js/my_clock.js
@@ -0,0 +1,33 @@
+function my_clock(el){
+    var today=new Date();
+    var h=today.getHours();
+    var m=today.getMinutes();
+    var s=today.getSeconds();
+    m=m>=10?m:('0'+m);
+    s=s>=10?s:('0'+s);
+    el.innerHTML = h+":"+m+":"+s;
+    setTimeout(function(){my_clock(el)}, 1000);
+}
+
+var clock_div = document.getElementById('clock_div');
+my_clock(clock_div);
+
+chrome.system.cpu.getInfo(function(info){
+    console.log(info);
+});
+
+chrome.system.memory.getInfo(function(info){
+    console.log(info);
+});
+
+chrome.system.storage.getInfo(function(info){
+    console.log(info);
+    //getAvailableCapacity接口不可用
+    /*
+    info.forEach(x=>(function(x){
+      chrome.system.storage.getAvailableCapacity(x.id, function(info){
+          console.log(info.availableCapacity);
+      });
+    }));
+    */
+});
