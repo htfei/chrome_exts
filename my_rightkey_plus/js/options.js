14ab396e73c5427dd9ba2a629a18c7391d430021
why2fly <flyte2012@163.com>
2018-02-12 11:31:47 +0800

    我的右键 增加导出配置文件功能

--- my_rightkey_plus/js/options.js
+++ my_rightkey_plus/js/options.js
@@ -89,3 +89,33 @@ document.getElementById('loadfile').onchange = function jsReadFiles() {
 		reader.readAsText(file);
 	}
 };
+
+// 导出localStorage到文件
+function fake_click(obj) {
+    var ev = document.createEvent("MouseEvents");
+    ev.initMouseEvent(
+        "click", true, false, window, 0, 0, 0, 0, 0
+        , false, false, false, false, 0, null
+        );
+    obj.dispatchEvent(ev);
+}
+function export_raw(name, data) {
+    var urlObject = window.URL || window.webkitURL || window;
+    var export_blob = new Blob([data]);
+    var save_link = document.createElementNS("http://www.w3.org/1999/xhtml", "a")
+    save_link.href = urlObject.createObjectURL(export_blob);
+    save_link.download = name;
+    fake_click(save_link);
+}
+document.getElementById('export_file').onclick = function(){
+	var name = "search.json";
+	var list = [];
+	for( var i = 0; i < localStorage.length; i++ ){
+		var key = localStorage.key(i);
+		var value = localStorage.getItem(key);
+		list.push({"key":key,"value":value});
+	}
+	var data =JSON.stringify({"tools":list});
+	console.log(data);
+	export_raw(name, data);
+}
