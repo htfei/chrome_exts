eb780d4f844f4d8adcb5a46b837af8b656ed9ba3
why2fly <flyte2012@163.com>
2018-02-27 14:47:16 +0800

    更新，完成单一rss源的展示与点击打开

--- RssPlus/js/my_clock.js
+++ RssPlus/js/my_clock.js
@@ -1,3 +1,4 @@
+
 function my_clock(el) {
     var today = new Date();
     var h = today.getHours();
@@ -11,7 +12,6 @@ function my_clock(el) {
 
 var clock_div = document.getElementById('clock_div');
 my_clock(clock_div);
-
 
 function httpRequest(url, callback) {
     var xhr = new XMLHttpRequest();
@@ -28,28 +28,29 @@ function showItem(rssxml) {
     //console.log(rssxml);
     var list = rssxml.getElementsByTagName('item');
     console.log(list[0]);
-    var div = '<div>';
+
+    var div = "<div>";
     for (i = 0; i < list.length; i++) {
-        var t = list[i].getElementsByTagName('title')[0].innerHTML;
-        var p = list[i].getElementsByTagName('pubDate')[0].innerHTML;
-        var l = list[i].getElementsByTagName('link')[0].innerHTML;
-        console.log(t,p,l);
-        div += '<a href="' + l + '">' + t + '</a><br/>';
-        //a.click(() => {chrome.tabs.create({url: l });});
+        var title = list[i].getElementsByTagName('title')[0].innerHTML;
+        //var pub = list[i].getElementsByTagName('pubDate')[0].innerHTML;
+        var link = list[i].getElementsByTagName('link')[0].innerHTML;
+        //console.log(t,p,l);
+        div += '<a class="list-group-item" href="'+ link +'" >'+ title +'</a>';
     }
-    div += '</div>';
+    div +="</div>"
+
     document.getElementById('item').innerHTML = div;
 }
 
 var rss = localStorage.rss;
-rss = rss ? rss : 'http://feed43.com/2770086871034514.xml';
+rss = rss ? rss : 'http://feed43.com/5123185481381181.xml';
 var url = rss;
 httpRequest(url, showItem);
 
 //绑定点击事件
-$('.del_btn').click(function(){
-    var obj = this.parentNode.parentNode;//input.td.tr
-    var key = obj.childNodes[1].childNodes[0].value ;//tr.td1.input.value
-    localStorage.removeItem(key);
-    location.reload();
-});
+window.onclick = function(e){
+    console.log(e.target);
+    if(e.target.href){
+        chrome.tabs.create({ url: e.target.href });
+    }       
+}
