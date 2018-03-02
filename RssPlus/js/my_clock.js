f6e37d03128ea5567d00abf874f0305f849f9148
why2fly <flyte2012@163.com>
2018-03-02 17:24:14 +0800

    加入websql

--- RssPlus/js/my_clock.js
+++ RssPlus/js/my_clock.js
@@ -1,4 +1,6 @@
 
+
+
 function my_clock(el) {
     var today = new Date();
     var h = today.getHours();
@@ -27,7 +29,7 @@ function httpRequest(url, callback) {
 function showItem(rssxml) {
     //console.log(rssxml);
     var list = rssxml.getElementsByTagName('item');
-    console.log(list[0]);
+    //console.log(list[0]);
 
     var div = "<div>";
     for (i = 0; i < list.length; i++) {
@@ -35,22 +37,64 @@ function showItem(rssxml) {
         //var pub = list[i].getElementsByTagName('pubDate')[0].innerHTML;
         var link = list[i].getElementsByTagName('link')[0].innerHTML;
         //console.log(t,p,l);
-        div += '<a class="list-group-item" href="'+ link +'" >'+ title +'</a>';
+        div += '<a class="list-group-item" href="' + link + '" >' + title + '</a>';
     }
-    div +="</div>"
+    div += "</div>"
 
     document.getElementById('item').innerHTML = div;
 }
 
-var rss = localStorage.rss;
-rss = rss ? rss : 'http://feed43.com/5123185481381181.xml';
-var url = rss;
-httpRequest(url, showItem);
+//var rss = localStorage.rss;
+//rss = rss ? rss : 'http://feed43.com/5123185481381181.xml';
+//var url = rss;
+//httpRequest(url, showItem);
 
 //绑定点击事件
-window.onclick = function(e){
-    console.log(e.target);
-    if(e.target.href){
+window.onclick = function (e) {
+    //console.log(e.target);
+    //console.log(e.target.getAttribute('data-rss'));
+    if(rss = e.target.getAttribute('data-rss')){
+        //加载某一个rss的内容
+        httpRequest(rss, showItem);
+        //更改head
+        document.getElementById('head').innerHTML = e.target.innerHTML;
+        document.getElementById('item').innerHTML = "loading ...";
+        //隐藏rss列表
+        var rssdiv = document.getElementById('rss');
+        rssdiv.parentNode.removeChild(rssdiv);
+    }
+    if (e.target.href) {
         chrome.tabs.create({ url: e.target.href });
-    }       
+    }
 }
+
+
+var myrss = "http://feed43.com/5123185481381181.xml";
+var myrsstitle = "网易新闻24H排行榜"
+var msg;
+//从websql中读取rss源，并显示到页面
+var db = openDatabase('myrssdb', '1.0', 'I can rss everthing !', 2 * 1024 * 1024);
+db.transaction(function (tx) {
+    //没有rss表则新建
+    tx.executeSql('CREATE TABLE IF NOT EXISTS MYRSS (rss unique, title)');
+    //插入一个rss源
+    //tx.executeSql('INSERT INTO MYRSS (rss,title) VALUES (?, ?)', [myrss, myrsstitle]);
+    //tx.executeSql('INSERT INTO MYRSS (rss,title) VALUES (?, ?)', ["http://feed43.com/5123185481381182.xml", "新浪24h"]);
+});
+
+//读取
+db.transaction(function (tx) {
+    tx.executeSql('SELECT * FROM MYRSS', [], function (tx, results) {
+        var len = results.rows.length;
+        //console.log(len);
+        var div = "<div>";
+        for (i = 0; i < len; i++) {
+            var rss = results.rows.item(i).rss;
+            var title = results.rows.item(i).title;
+            //console.log(title);
+            div += '<a class="list-group-item" data-rss="' + rss + '" >' + title + '</a>';
+        }
+        div += "<div>";
+        document.getElementById('rss').innerHTML = div;
+    }, null);
+});
