70462d4d06bf5c89af119f5d0924de6e3fec9b6f
Terry <flyte2012@163.com>
2018-03-08 00:33:07 +0800

    rss订阅器更新，mysql储存

--- RssPlus/js/my_clock.js
+++ RssPlus/js/my_clock.js
@@ -1,77 +1,5 @@
-
-
-
-function my_clock(el) {
-    var today = new Date();
-    var h = today.getHours();
-    var m = today.getMinutes();
-    var s = today.getSeconds();
-    m = m >= 10 ? m : ('0' + m);
-    s = s >= 10 ? s : ('0' + s);
-    el.innerHTML = h + ":" + m + ":" + s;
-    setTimeout(function () { my_clock(el) }, 1000);
-}
-
-var clock_div = document.getElementById('clock_div');
-my_clock(clock_div);
-
-function httpRequest(url, callback) {
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", url, true);
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState == 4) {
-            callback(xhr.responseXML);//此处返回xml对象
-        }
-    }
-    xhr.send();
-}
-
-function showItem(rssxml) {
-    //console.log(rssxml);
-    var list = rssxml.getElementsByTagName('item');
-    //console.log(list[0]);
-
-    var div = "<div>";
-    for (i = 0; i < list.length; i++) {
-        var title = list[i].getElementsByTagName('title')[0].innerHTML;
-        //var pub = list[i].getElementsByTagName('pubDate')[0].innerHTML;
-        var link = list[i].getElementsByTagName('link')[0].innerHTML;
-        //console.log(t,p,l);
-        div += '<a class="list-group-item" href="' + link + '" >' + title + '</a>';
-    }
-    div += "</div>"
-
-    document.getElementById('item').innerHTML = div;
-}
-
-//var rss = localStorage.rss;
-//rss = rss ? rss : 'http://feed43.com/5123185481381181.xml';
-//var url = rss;
-//httpRequest(url, showItem);
-
-//绑定点击事件
-window.onclick = function (e) {
-    //console.log(e.target);
-    //console.log(e.target.getAttribute('data-rss'));
-    if(rss = e.target.getAttribute('data-rss')){
-        //加载某一个rss的内容
-        httpRequest(rss, showItem);
-        //更改head
-        document.getElementById('head').innerHTML = e.target.innerHTML;
-        document.getElementById('item').innerHTML = "loading ...";
-        //隐藏rss列表
-        var rssdiv = document.getElementById('rss');
-        rssdiv.parentNode.removeChild(rssdiv);
-    }
-    if (e.target.href) {
-        chrome.tabs.create({ url: e.target.href });
-    }
-}
-
-
 var myrss = "http://feed43.com/5123185481381181.xml";
 var myrsstitle = "网易新闻24H排行榜"
-var msg;
 //从websql中读取rss源，并显示到页面
 var db = openDatabase('myrssdb', '1.0', 'I can rss everthing !', 2 * 1024 * 1024);
 db.transaction(function (tx) {
@@ -79,7 +7,7 @@ db.transaction(function (tx) {
     tx.executeSql('CREATE TABLE IF NOT EXISTS MYRSS (rss unique, title)');
     //插入一个rss源
     //tx.executeSql('INSERT INTO MYRSS (rss,title) VALUES (?, ?)', [myrss, myrsstitle]);
-    //tx.executeSql('INSERT INTO MYRSS (rss,title) VALUES (?, ?)', ["http://feed43.com/5123185481381182.xml", "新浪24h"]);
+    //tx.executeSql('INSERT INTO MYRSS (rss,title) VALUES (?, ?)', ["http://www.feed43.com/2770086871034514.xml", "抽屉24h最热"]);
 });
 
 //读取
@@ -92,9 +20,80 @@ db.transaction(function (tx) {
             var rss = results.rows.item(i).rss;
             var title = results.rows.item(i).title;
             //console.log(title);
-            div += '<a class="list-group-item" data-rss="' + rss + '" >' + title + '</a>';
+            div += '<li class="list-group-item btn btn-default" style="text-align: left;" data-rss="' + rss + '" >' + title + '</li>';
         }
         div += "<div>";
         document.getElementById('rss').innerHTML = div;
     }, null);
 });
+
+//请求RSS源
+function httpRequest(url, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", url, true);
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4) {
+            var feeds = callback(xhr.responseXML);//此处返回xml对象
+            console.log(feeds);
+            //console.log(feeds.length);
+            //没有rss表则新建
+            db.transaction(function (tx) {         
+                tx.executeSql('CREATE TABLE IF NOT EXISTS Feeds (url unique, title)');
+                 //插入一个rss源
+                for (i = 0; i < feeds.length; i++) {
+                    tx.executeSql('INSERT INTO Feeds (url,title) VALUES (?,?)',feeds[i],null,null);
+                    //null -> function (tx, error) { console.log('添加数据失败: ' + error.message)}   
+                }
+            });
+        }
+    }
+    xhr.send();
+}
+
+//解析并显示最新feeds
+function showItem(rssxml) {
+    //console.log(rssxml);
+    var list = rssxml.getElementsByTagName('item');
+    //console.log(list[0]);
+    var feeds = [];
+    var div = "<div>";
+    for (i = 0; i < list.length; i++) {
+        var title = list[i].getElementsByTagName('title')[0].innerHTML;
+        var pubdate = list[i].getElementsByTagName('pubDate')[0].innerHTML;
+        var link = list[i].getElementsByTagName('link')[0].innerHTML;
+        //console.log(t,p,l);
+        div += '<a class="list-group-item" href="' + link + '" >' + title + '</a>';
+        feeds.push([link, title]);
+    }
+    div += "</div>"
+
+    document.getElementById('item').innerHTML = div;
+    return feeds;
+}
+
+//var rss = localStorage.rss;
+//rss = rss ? rss : 'http://feed43.com/5123185481381181.xml';
+//var url = rss;
+//httpRequest(url, showItem);
+
+//绑定点击事件
+window.onclick = function (e) {
+    //console.log(e.target);
+    //console.log(e.target.getAttribute('data-rss'));
+    if (rss = e.target.getAttribute('data-rss')) {
+        //加载某一个rss的内容
+        document.getElementById('item').innerHTML = "loading ...";
+        httpRequest(rss, showItem);
+        //更改head
+        div = '<p class="list-group-item">' + e.target.innerHTML + '</p>';
+        document.getElementById('head').innerHTML = div;
+        //隐藏rss列表
+        var rssdiv = document.getElementById('rss');
+        rssdiv.parentNode.removeChild(rssdiv);
+    }
+    if (e.target.href) {
+        chrome.tabs.create({ url: e.target.href });
+    }
+}
+
+
