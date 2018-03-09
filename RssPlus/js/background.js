2774d9a12e45265608b3518acfb431468fd91fb2
why2fly <flyte2012@163.com>
2018-03-09 16:55:30 +0800

    初步完成可食用！后台定期请求rss，popup页面只负责显示，加入未读数量badge，点击item的badge可直接标记为已读，点击rss列表的bage可将整个rss源的items标记为已读！

--- RssPlus/js/background.js
+++ RssPlus/js/background.js
@@ -1,21 +1,69 @@
-
 //定时(5min)请求rss源，更新websql
 var db = openDatabase('myrssdb', '1.0', 'I can rss everthing !', 2 * 1024 * 1024);
 db.transaction(function (tx) {
-    //没有rss表则新建
-    //tx.executeSql('CREATE TABLE IF NOT EXISTS Rss (rss unique, title)');
-    //插入一个rss源
-    //var myrss = "http://feed43.com/5123185481381181.xml";
-    //var myrsstitle = "网易新闻24H排行榜"
-    //tx.executeSql('INSERT INTO Rss (rss,title) VALUES (?, ?)', [myrss, myrsstitle]);
-    //tx.executeSql('INSERT INTO Rss (rss,title) VALUES (?, ?)', ["http://www.feed43.com/2770086871034514.xml", "抽屉24h最热"]);
 
+    //新建表
+    tx.executeSql('CREATE TABLE IF NOT EXISTS Rss (rss unique, title,unreadNums)');
+    tx.executeSql('CREATE TABLE IF NOT EXISTS Feeds (url unique, title, pubtimestamp,isread,rssUrl)');
+
+    //插入一个rss源
+    tx.executeSql('INSERT INTO Rss (rss,title) VALUES (?, ?)', ["https://feed43.com/5123185481381181.xml", "网易新闻24H排行榜"]);
+    tx.executeSql('INSERT INTO Rss (rss,title) VALUES (?, ?)', ["https://feed43.com/2770086871034514.xml", "抽屉24h最热"]);
+    tx.executeSql('INSERT INTO Rss (rss,title) VALUES (?, ?)', ["https://feed43.com/3680851688572686.xml", "天涯实时热帖榜"]);
+    tx.executeSql('INSERT INTO Rss (rss,title) VALUES (?, ?)', ["https://feed43.com/4677052036764060.xml", "光谷社区"]);
+    tx.executeSql('INSERT INTO Rss (rss,title) VALUES (?, ?)', ["https://feed43.com/1011517213012743.xml", "武汉房管局公告"]);
+    tx.executeSql('INSERT INTO Rss (rss,title) VALUES (?, ?)', ["https://feed43.com/3864107213381284.xml", "武汉亿房论坛"]);
+
+    //更新值
+    //tx.executeSql('UPDATE Rss SET rss=? where rowid=2', ["http://feed43.com/2770086871034514.xml"]);
+    //tx.executeSql('UPDATE Feeds SET rssUrl=?', ["http://feed43.com/5123185481381181.xml"]);
+    //tx.executeSql('UPDATE Feeds SET rssUrl=? where rowid = 363 ', ["https://feed43.com/3680851688572686.xml"]);
+    
+
+    //删除表
     //tx.executeSql('drop table MYRSS');
-    //tx.executeSql('CREATE TABLE IF NOT EXISTS Feeds (url unique, title, pubtimestamp)');
+      
+    //增加列
     //tx.executeSql("ALTER TABLE Feeds ADD COLUMN pubtimestamp INT");
-    //tx.executeSql("ALTER TABLE Feeds ADD COLUMN isread BOOL");
+    //tx.executeSql("ALTER TABLE Feeds ADD COLUMN rssUrl");
+
 });
 
+//储存items到websql
+function items2websql(items){
+    db.transaction(function (tx) {
+        for (i = 0; i < items.length; i++) {
+            tx.executeSql('INSERT OR IGNORE INTO Feeds (url,title,pubtimestamp,rssUrl) VALUES (?,?,?,?)', items[i],
+                null,
+                function (tx, error) {
+                    console.log('添加数据失败: ' + error.message)
+                });
+        }
+    });
+}
+
+//解析items
+function showItem(rssxml, rssurl) {
+    //console.log(rssxml); //err console.log(rssxml.innerHTML);
+    if (rssxml) {
+        var list = rssxml.getElementsByTagName('item');
+        var items = [];
+        for (i = 0; i < list.length; i++) {
+            var title = list[i].getElementsByTagName('title')[0].innerHTML;
+            var pubdate = list[i].getElementsByTagName('pubDate')[0].innerHTML;
+            var pubtimestamp = Math.round(new Date(pubdate).getTime() / 1000);
+            var itemurl = list[i].getElementsByTagName('link')[0].innerHTML;
+            //console.log("item",i,title);
+            //executeSql第一条执行完毕之前for循环已经结束了，故只插入了一条记录，需先保存起来在一次插入
+            items.push([itemurl, title, pubtimestamp, rssurl]);
+        }
+        items2websql(items);
+        console.log(rssurl, "更新成功！");
+
+    } else {
+        console.log(rssurl, "更新失败!");
+    }
+}
 
 
 //请求RSS源
@@ -24,25 +72,32 @@ function httpRequest(url, callback) {
     xhr.open("GET", url, true);
     xhr.onreadystatechange = function () {
         if (xhr.readyState == 4) {
-            callback(xhr.responseXML); //此处返回xml对象
+            callback(xhr.responseXML, url); //此处返回xml对象
         }
     }
     xhr.send();
 }
 
-function makeItemRead(itemUrl) {
+//读取rss列表并依次执行请求
+function rss_request() {
+    console.log("开始请求rss源数据...")
     db.transaction(function (tx) {
-        tx.executeSql('update Feeds set isread = 1 where url = ?', [itemUrl], null, function (tx, error) {
-            console.log('添加数据失败: ' + error.message)
-        });
+        tx.executeSql('SELECT rss FROM Rss', [], function (tx, results) {
+                var len = results.rows.length;
+                //console.log(len);
+                for (i = 0; i < len; i++) {
+                    var rssUrl = results.rows.item(i).rss;
+                    var title = results.rows.item(i).title;
+                    //console.log("请求",title,rssUrl);
+                    httpRequest(rssUrl, showItem);
+                }
+            },
+            function (tx, error) {
+                console.log('查询数据失败: ', error.message)
+            });
     });
+    //console.log("请求rss源数据完毕！5mim后再次执行！")
 }
 
-chrome.runtime.onMessage.addListener(function(message, sender, sendResponse){
-    if(message == 'Hello'){
-        sendResponse(rssstr);
-    }
-    if(message.cmd == 'makeItemRead'){
-        makeItemRead(message.itemUrl);
-    }
-});
+//rss_request();
+setInterval("rss_request()",300000);//5min周期执行
--- RssPlus/js/my_clock.js
+++ RssPlus/js/my_clock.js
@@ -1,161 +1,114 @@
 //从websql中读取rss源，并显示到页面
 var db = openDatabase('myrssdb', '1.0', 'I can rss everthing !', 2 * 1024 * 1024);
-db.transaction(function (tx) {
-    //没有rss表则新建
-    //tx.executeSql('CREATE TABLE IF NOT EXISTS Rss (rss unique, title)');
-    //插入一个rss源
-    //var myrss = "http://feed43.com/5123185481381181.xml";
-    //var myrsstitle = "网易新闻24H排行榜"
-    //tx.executeSql('INSERT INTO Rss (rss,title) VALUES (?, ?)', [myrss, myrsstitle]);
-    //tx.executeSql('INSERT INTO Rss (rss,title) VALUES (?, ?)', ["http://www.feed43.com/2770086871034514.xml", "抽屉24h最热"]);
-
-    //tx.executeSql('drop table MYRSS');
-    //tx.executeSql('CREATE TABLE IF NOT EXISTS Feeds (url unique, title, pubtimestamp)');
-    //tx.executeSql("ALTER TABLE Feeds ADD COLUMN pubtimestamp INT");
-    tx.executeSql("ALTER TABLE Feeds ADD COLUMN rssUrl");
-});
 
 var rssstr = "";
 //页面加载时读取rss源列表
 function loadRssfromWebsql() {
     db.transaction(function (tx) {
-        tx.executeSql('SELECT * FROM Rss', [], function (tx, results) {
-            var len = results.rows.length;
-            //console.log(len);
-            var div = "<div>";
-            for (i = 0; i < len; i++) {
-                var rss = results.rows.item(i).rss;
-                var title = results.rows.item(i).title;
-                //console.log(title);
-                div += '<li class="list-group-item btn btn-default" style="text-align: left;" data-rss="' + rss + '" >' + title + '</li>';
-            }
-            div += "<div>";
-            document.getElementById('rss').innerHTML = div;
-            rssstr = div;
-            //console.log(rssstr);
-        }, null);
+        tx.executeSql('SELECT * FROM Rss', [],
+            function (tx, results) {
+                var len = results.rows.length;
+                //console.log(len);
+                rssstr = "<div>";
+                for (i = 0; i < len; i++) {
+                    var rss = results.rows.item(i).rss;
+                    var title = results.rows.item(i).title;
+                    var unreadNums = results.rows.item(i).unreadNums;
+                    tx.executeSql('UPDATE Rss SET unreadNums=(SELECT COUNT(*) FROM Feeds WHERE rssUrl=? AND isread IS NULL) WHERE rss=?', [rss, rss]);
+                    //console.log(title);
+                    if (unreadNums && unreadNums != 0) {
+                        rssstr += '<li class="list-group-item btn btn-default" style="text-align: left;" data-rss="' + rss + '" ><span class="badge pull-right" title="将所有项标记为已读">' + unreadNums + '</span>' + title + '</li>';
+                    } else {
+                        rssstr += '<li class="list-group-item btn btn-default" style="text-align: left;" data-rss="' + rss + '" >' + title + '</li>';
+                    }
+                }
+                rssstr += "</div>";
+                document.getElementById('rss').innerHTML = rssstr;
+                //console.log(rssstr);
+            },null);
     });
 }
 loadRssfromWebsql();
 
+var itemstr = "";
+
 function loadItemsfromWebsql(rssUrl) {
     db.transaction(function (tx) {
-        tx.executeSql('SELECT * FROM Feeds where rssUrl = ?', [rssUrl], function (tx, results) {
+        tx.executeSql('SELECT * FROM Feeds where rssUrl = ? ORDER BY pubtimestamp DESC LIMIT 10', [rssUrl], function (tx, results) {
             var len = results.rows.length;
-            //console.log(len);
-            var div = "<div>";
-            for (i = 0; i < len; i++) {
-                var rss = results.rows.item(i).rss;
-                var title = results.rows.item(i).title;
-                //console.log(title);
-                div += '<li class="list-group-item btn btn-default" style="text-align: left;" data-rss="' + rss + '" >' + title + '</li>';
+            console.log(len);
+            if (len) {
+                itemstr = "<div>";
+                for (i = 0; i < len; i++) {
+                    var itemurl = results.rows.item(i).url;
+                    var title = results.rows.item(i).title;
+                    var isread = results.rows.item(i).isread;
+                    if (isread == 1) {
+                        itemstr += '<a class="list-group-item" href="' + itemurl + '" >' + title + '</a>';
+                    } else {
+                        itemstr += '<a class="list-group-item" href="' + itemurl + '" ><span class="badge pull-right" title="标记为已读">新</span>' + title + '</a>';
+                    }
+                }
+                itemstr += "</div>";
+                document.getElementById('item').innerHTML = itemstr;
+            } else {
+                document.getElementById('item').innerHTML = '<a class="list-group-item">暂无更新！</a>';
             }
-            div += "<div>";
-            document.getElementById('rss').innerHTML = div;
-            rssstr = div;
-            //console.log(rssstr);
-        }, null);
-    });
-}
 
-//解析并显示最新feeds
-function showItem(rssxml) {
-    console.log(rssxml);
-    //err console.log(rssxml.innerHTML);
-    var feeds = [];
-    if (rssxml) {
-        var list = rssxml.getElementsByTagName('item');
-
-        for (i = 0; i < list.length; i++) {
-            var title = list[i].getElementsByTagName('title')[0].innerHTML;
-            var pubdate = list[i].getElementsByTagName('pubDate')[0].innerHTML;
-            var pubtimestamp = Math.round(new Date(pubdate).getTime() / 1000);
-            var itemurl = list[i].getElementsByTagName('link')[0].innerHTML;
-            //console.log(t,p,l);
-            feeds.push([itemurl, title, pubtimestamp]);
-            //显示加载项
-            document.getElementById('item').innerHTML = '<a class="list-group-item" href="' + itemurl + '" >' + title + '</a>';
-        }
-
-        for (i = 0; i < list.length; i++) {
-            //写成函数会导致赋值延后
-            db.transaction(function (tx) {
-                //将item写入websq
-                tx.executeSql('INSERT OR IGNORE INTO Feeds (url,title,pubtimestamp) VALUES (?,?,?)', [itemurl, title, pubtimestamp],
-                    null,
-                    function (tx, error) {
-                        console.log('添加数据失败: ' + error.message)
-                    });
-                //读取isread是否已读，未读则加载“新”标记
-                tx.executeSql('SELECT isread FROM Feeds where url = ?', [itemurl],
-                    function (tx, results) {
-                        var isread = results.rows.item(0).isread;
-                        //console.log(isread);
-                        if (isread == 1) {
-                            itemdiv +=
-                        } else {
-                            itemdiv += '<a class="list-group-item" href="' + itemurl + '" ><span class="badge pull-right">新</span>' + title + '</a>';
-                        }
-                    },
-                    null);
-            });
-        }
-
-    } else {
-        document.getElementById('item').innerHTML = "加载失败";
-    }
-    return feeds;
-}
-
-//var rss = localStorage.rss;
-//rss = rss ? rss : 'http://feed43.com/5123185481381181.xml';
-//var url = rss;
-//httpRequest(url, showItem);
-
-function makeItemRead(itemUrl) {
-    db.transaction(function (tx) {
-        tx.executeSql('update Feeds set isread = 1 where url = ?', [itemUrl], null, function (tx, error) {
-            console.log('添加数据失败: ' + error.message)
+            //console.log(itemstr);
+        }, 
+        function (tx, error) {
+            console.log('失败!' , error.message)
         });
     });
 }
 
+//标记为已读
+function makeItemRead(itemUrl) {
+    db.transaction(function (tx) {
+        tx.executeSql('update Feeds set isread = 1 where url = ?', [itemUrl], null, null);
+    });
+}
+//标记为已读
+function makeRssItemsRead(rssUrl) {
+    db.transaction(function (tx) {
+        tx.executeSql('update Feeds set isread = 1 where rssUrl = ?', [rssUrl], null, null);
+        tx.executeSql('update Rss set unreadNums = 0 where rss = ?', [rssUrl], null, null);
+    });
+}
 
 
 //绑定点击事件
 window.onclick = function (e) {
     //console.log(e.target);
-    //console.log(e.target.getAttribute('data-rss'));
     if (rssUrl = e.target.getAttribute('data-rss')) {
         //加载某一个rss的内容
-        document.getElementById('item').innerHTML = "loading ...";
-        //httpRequest(rssUrl, showItem);
         loadItemsfromWebsql(rssUrl);
-        //更改head
+        //更改head,隐藏rss列表
         div = '<p class="list-group-item">' + e.target.innerHTML + '</p>';
         document.getElementById('head').innerHTML = div;
-        //隐藏rss列表
         document.getElementById('rss').innerHTML = "";
     }
     if (e.target.href) {
         //将对应item标记为已读
-        //makeItemRead(e.target.href);
-        //var msg = [];
-        chrome.runtime.sendMessage({
-            'cmd': "makeItemRead",
-            'itemUrl': e.target.href
-        }, null);
-        //chrome.tabs.create({ url: e.target.href });
-        //console.log(e.target.href);
+        makeItemRead(e.target.href);
+        chrome.tabs.create({
+            url: e.target.href
+        });
     }
     if (e.target.id == 'head' || e.target.parentNode.id == 'head') {
-        //this.console.log(e.target);
-        //this.console.log(rssstr);
         //隐藏head,隐藏items列表,加载rss列表
         document.getElementById('head').innerHTML = "";
         document.getElementById('item').innerHTML = "";
         document.getElementById('rss').innerHTML = rssstr;
+    }
+    //将对应item标记为已读
+    if (e.target.getAttribute('title') == '标记为已读') {
+        makeItemRead(e.target.parentNode.href);
+    }
+    if (e.target.getAttribute('title') == '将所有项标记为已读') {
+        makeRssItemsRead(e.target.parentNode.getAttribute('data-rss'));
     } else {
-        //this.console.log(e.target);
+        this.console.log(e.target);
     }
 }
