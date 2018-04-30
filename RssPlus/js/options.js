87856888fa4ff2a54e822e8c0ae599973f96ba85
Terry <flyte2012@163.com>
2018-04-30 20:12:17 +0800

    修复目录和名称显示错位问题，完成编辑功能

--- RssPlus/js/options.js
+++ RssPlus/js/options.js
@@ -62,15 +62,25 @@ function loadRss() {
         tx.executeSql('SELECT * FROM Rss', [],
             function (tx, results) {
                 var divstr = '<table class="table table-striped table-hover table-condensed">' +
-                '<caption>管理订阅的RSS源(开发中)</caption><thead><tr><th>名称</th><th>目录</th><th>RSS链接</th></tr></thead><tbody>';
+                '<caption>管理订阅的RSS源(开发中)</caption><thead><tr><th>目录</th><th>名称</th><th>RSS链接</th></tr></thead><tbody>';
                 for (i = 0; i < results.rows.length; i++) {
                     var rss = results.rows.item(i).rss;
                     var title = results.rows.item(i).title;
                     var dir = results.rows.item(i).dir;
-                    divstr += '<tr><td>' + dir + '</td><td>' + title + '</td><td>' + rss + '</td><td><input type="button" class="edit" value="编辑" /></td></tr>';
+                    divstr += '<tr><td>' + dir + '</td><td>' + title + '</td><td>' + rss + '</td><td><input type="button" class="up_btn" value="编辑" /></td></tr>';
                 }
                 divstr += '</tbody></table>';
                 document.getElementById('rss').innerHTML = divstr ;
+                //更新某一项键值对
+                $('.up_btn').click(function(){
+                //此处不能使用 document.getElementsByClassName('up_btn').onclick = function(){ ，因为得到的是数组，无法直接调用onclick,需要循环绑定
+                //该函数放到loadRss()外面无效，猜测executeSql查询数据库处理延后，导致该函数先执行时'.up_btn'还未生成，绑定失败，但是无提示信息？
+                    var obj = this.parentNode.parentNode;//input.td.tr
+                    document.getElementById('rssurl').value = obj.childNodes[2].innerHTML;
+                    document.getElementById('rsstitle').value = obj.childNodes[1].innerHTML;
+                    document.getElementById('rssdir').value = obj.childNodes[0].innerHTML;
+                    console.log(obj);
+                });
             },
             null
         );
@@ -79,13 +89,3 @@ function loadRss() {
 
 loadRss();
 
-//更新某一项键值对
-$('.edit').click(function(){
-//document.getElementsByClassName('edit').onclick = function(){
-    var obj = this.parentNode.parentNode;//input.td.tr
-    document.getElementById('rssurl').value = obj.childNodes[2].value;
-    document.getElementById('rsstitle').value = obj.childNodes[0].value;
-    document.getElementById('rssdir').value = obj.childNodes[1].value;
-    console.log(obj);
-//}
-});
