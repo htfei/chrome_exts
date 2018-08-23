38549955294c3c4591be6c72e3b0eb960ed9dc0e
why2fly <why2fly@aliyun.com>
2018-08-24 00:05:33 +0800

    temp

--- Bookmarklet合集/选中区操作.js
+++ Bookmarklet合集/选中区操作.js
@@ -1,49 +1,36 @@
-// ==UserScript==
-// @name         选中区操作
-// @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  指定区域的指定操作!
-// @author       Terry
-// @match        *://*/*
-// @grant        none
-// @require       http://code.jquery.com/jquery-3.1.0.min.js
-// ==/UserScript==
-
-(function () {
-    'use strict';
-
-    //公共函数
-    function get_selector(node){
-        var s =  (node.tagName) + (node.className?("."+node.className):"");
+javascript: (() => {
+    /*公共函数*/
+    function get_selector(node) {
+        var s = (node.tagName) + (node.className ? ("." + node.className) : "");
         s = s.split(' ').join('.');
         return s;
     }
 
-    function get_id_selector(node){
-        var s =  (node.tagName) + (node.id?("#"+node.id):"") + (node.className?("."+node.className):"");
+    function get_id_selector(node) {
+        var s = (node.tagName) + (node.id ? ("#" + node.id) : "") + (node.className ? ("." + node.className) : "");
         s = s.split(' ').join('.');
         return s;
     }
 
-    function is_only_selector(s){
-        if($(s).length != 1){
+    function is_only_selector(s) {
+        if ($(s).length != 1) {
             return false;
         }
         return true;
     }
 
-    function get_only_selector(node){
+    function get_only_selector(node) {
         var s = get_selector(node);
 
-        if(is_only_selector(s)){
+        if (is_only_selector(s)) {
             return s;
         }
-        while(node.parentNode.tagName !== 'HTML'){
+        while (node.parentNode.tagName !== 'HTML') {
             var fn = node.parentNode;
             var fs = get_selector(fn);
             s = fs + ">" + s;
-            if(is_only_selector(s)){
-                return s;/* 可以精确定位到所选的元素了 */
+            if (is_only_selector(s)) {
+                return s; /* 可以精确定位到所选的元素了 */
             }
             /* 依次往上层遍历 */
             node = node.parentNode;
@@ -53,18 +40,26 @@
         return s;
     }
 
-    function show_red_border(s){
-        [].map.call($(s),function(snode){snode.setAttribute('style','border:1px solid #F00');});
+    function show_red_border(s) {
+        [].map.call($(s), function (snode) {
+            snode.setAttribute('style', 'border:1px solid #F00');
+        });
     }
 
     /* 新添加的操作主界面div */
-    function createMydiv(){
+    function createMydiv() {
         var div = document.createElement('div');
         div.id = 'mydiv';
         div.style.cssText = "width:300px; height:300px; position:fixed; top:20%; right:40px";
-        div.innerHTML = 
+        div.innerHTML =
             '<button id="gotop">返回顶部</button><br/>' +
-            '<button id="gps">开始定位</button><br/>' +
+            'items:<br/><input></input><button class="gps 1">开始定位</button><br/>' +
+            'items.title:<br/><input></input><button class="gps 2">开始定位</button><br/>' +
+            'items.url:<br/><input></input><button class="gps 3">开始定位</button><br/>' +
+            'items.desc:<br/><input></input><button class="gps 4">开始定位</button><br/>' +
+            'items.author:<br/><input></input><button class="gps 5">开始定位</button><br/>' +
+            'items.time:<br/><input></input><button class="gps 6">开始定位</button><br/>' +
+            'items.reply:<br/><input></input><button class="gps 7">开始定位</button><br/>' +
             '<button id="up">上</button>' +
             '<button id="down">下</button>' +
             '<button id="left">左</button>' +
@@ -72,12 +67,13 @@
             '<div id="log"></div>';
         document.body.appendChild(div);
 
-        //绑定点击事件
+        /*绑定点击事件*/
         document.getElementById('gotop').onclick = function () {
             window.scrollTo(0, 0);
         };
-        document.getElementById('gps').onclick = function () {
-            localStorage.gps = localStorage.gps ? localStorage.gps : 1;
+        document.getElementsByClassName('gps').onclick = function () {
+            localStorage.gps = this.className;
+            $(this.parentNode).find('input').get(0).text() = this.className;
         };
         document.getElementById('up').onclick = function () {
             var s = get_selector(show_node.parentNode);
@@ -85,8 +81,8 @@
         };
     }
 
-    function show_log(str){
-        document.getElementById('log').innerHTML = "<p>"+str + "</p>";
+    function show_log(str) {
+        document.getElementById('log').innerHTML = "<p>" + str + "</p>";
     }
 
     var mydiv = document.getElementById("mydiv");
@@ -94,7 +90,6 @@
         mydiv.parentNode.removeChild(mydiv);
     } else {
 
-        var s = "";
         var show_node = null;
         createMydiv();
 
@@ -102,11 +97,11 @@
         window.onmouseup = function (e) {
             var targ = e.target;
 
-            if(targ.id == "mydiv" || targ.parentNode.id == "mydiv"){
-                return ;
+            if (targ.id == "mydiv" || targ.parentNode.id == "mydiv") {
+                return;
             }
-            if(targ.tagName == "INPUT" || targ.tagName == "input"){
-                return ;
+            if (targ.tagName == "INPUT" || targ.tagName == "input") {
+                return;
             }
 
             /* 旧节点移除高亮，新节点添加高亮 */
@@ -124,4 +119,5 @@
         };
 
     }
+
 })();
